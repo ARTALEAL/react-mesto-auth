1f9db62a132284d320e44822a3caa46e79211ff5
ARTALEAL <artaleal@yandex.ru>
2023-01-26 18:39:28 +0300

    develop: register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,41 @@
+import { Link } from 'react-router-dom';
+import useForm from '../hooks/useForm';
+
 function Register() {
-  const st = { color: 'white' };
-  return <h1 style={st}>Register</h1>;
+  const { enteredValues, errors, handleChange } = useForm({});
+  return (
+    <div className="auth">
+      <h2 className="auth__title">Регистрация</h2>
+      <form className="form auth__form" noValidate>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          id="email"
+          autoComplete="email"
+          value={enteredValues.email || ''}
+          onChange={handleChange}
+          required
+        />
+        <span className="auth__error">{errors.email}</span>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Пароль"
+          autoComplete="password"
+          value={enteredValues.password || ''}
+          onChange={handleChange}
+          required
+        />
+        <span className="auth__error">{errors.password}</span>
+        <button type="submit">Зарегистрироваться</button>
+      </form>
+      <Link to="/sign-in" className="auth__login-hint">
+        Уже зарегистрированы? Войти
+      </Link>
+    </div>
+  );
 }
 
 export default Register;
